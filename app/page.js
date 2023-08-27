e924cd4aee8d7b73089e4383136d4505822ce051
Faye <dominic@stilma.net>
2023-08-27 04:48:59 +0200

    remove unused stuff

--- app/page.js
+++ app/page.js
@@ -1,22 +1,9 @@
-"use client";
-import { useState } from "react";
-
 export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
-
-      <a href="https://backend.headpat.de/api/connect/discord/callback">
-        {" "}
-        Login with Discord
-      </a>
+      <h1>Hi :3</h1>
+      <span>Other text!</span>
     </div>
   );
 }
--- app/user/[uniqueId]/page.js
+++ app/user/[uniqueId]/page.js
@@ -1,12 +1,4 @@
-'use client';
-import { useState } from 'react';
-
 export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
 
   return (
     <div>
