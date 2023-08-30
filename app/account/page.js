94d7e1383dbd66e67db211d716408de5d43e4ce9
Faye <dominic@stilma.net>
2023-08-30 23:47:12 +0200

    remove soon on nsfw

--- app/account/page.js
+++ app/account/page.js
@@ -372,10 +372,14 @@ export default function AccountPage() {
           <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
             <div>
               <h2 className="text-base font-semibold leading-7 text-white">
-                Enable NSFW (Soon)
+                Enable NSFW
               </h2>
               <p className="mt-1 text-sm leading-6 text-gray-400">
                 Checking this box will enable NSFW images. 18+ only.
+              </p>
+              <p className="text-sm leading-6 text-gray-400">
+                Anyone under the age of 18 caught viewing NSFW content will be
+                suspended.
               </p>
             </div>
 
