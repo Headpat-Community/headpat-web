9f451825294ecbdcdca8834168ed8accbd6bf399
Faye <dominic@stilma.net>
2023-08-31 18:09:28 +0200

    add uploading change

--- app/account/upload/page.js
+++ app/account/upload/page.js
@@ -239,8 +239,10 @@ export default function UploadPage() {
               type="submit"
               value="Submit"
               className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              disabled={isUploading} // Disable the button if isUploading is true
             >
-              Save
+              {isUploading ? "Uploading..." : "Save"}{" "}
+              {/* Show different text based on the upload state */}
             </button>
           </div>
         </form>
