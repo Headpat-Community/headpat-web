7fa8c31d06bed56ea553b2974d106505ae84e459
Faye <dominic@stilma.net>
2023-09-21 22:37:25 +0200

    finish badge page

--- components/account/badgePage.js
+++ components/account/badgePage.js
@@ -1,14 +1,40 @@
-import Image from "next/image";
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState, useRef } from "react";
 
 export default function BadgePageComponent() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
   const [deliverAtEurofurence, setDeliverAtEurofurence] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedLanguages, setSelectedLanguages] = useState("");
+  const displaynameRef = useRef("");
+  const pronounsRef = useRef("");
+  const addressRef = useRef("");
+  const cityRef = useRef("");
+  const countryRef = useRef("");
+  const stateRef = useRef("");
+  const postalcodeRef = useRef("");
 
-  const handleCheckboxChange = (event) => {
+  useEffect(() => {
+    const checkboxes = document.querySelectorAll(
+      "input[type=checkbox][name=languages]"
+    );
+    const hiddenInput = document.getElementById("selectedLanguages");
+
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", function () {
+        const selectedLanguages = Array.from(checkboxes)
+          .filter((c) => c.checked)
+          .map((c) => c.value)
+          .join(", ");
+        hiddenInput.value = selectedLanguages;
+        setSelectedLanguages(selectedLanguages);
+      });
+    });
+  }, []);
+
+  const handleCheckboxChange = useCallback((event) => {
     setDeliverAtEurofurence(event.target.checked);
-  };
+  }, []);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -56,6 +82,19 @@ export default function BadgePageComponent() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const checkboxes = document.querySelectorAll(
+      "input[type=checkbox][name=languages]"
+    );
+    const selectedCheckboxes = Array.from(checkboxes).filter((c) => c.checked);
+    if (selectedCheckboxes.length === 0) {
+      setError("Bitte eine sprache auswählen.");
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+      return;
+    }
+
+    event.preventDefault();
 
     const formData = new FormData();
     formData.append("files.img", selectedFile);
@@ -79,39 +118,62 @@ export default function BadgePageComponent() {
       const userResponseData = await userResponse.json();
       const userId = userResponseData.id;
 
-      formData.append(
-        "data",
-        JSON.stringify({
-          users_permissions_user: userId,
-          displayname: displayname.value,
-          address: address.value,
-          city: city.value,
-          country: country.value,
-          state: state.value,
-          postalcode: postalcode.value,
-          deliver_at_eurofurence: deliver_at_eurofurence.checked,
-        })
+      const response = await fetch(
+        `https://backend.headpat.de/api/badges?filters[users_permissions_user][id][$eq]=${userId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_DOMAIN_API_KEY}`,
+          },
+        }
       );
 
-      setIsUploading(true); // Set isUploading to true before making the API call
-
-      const response = await fetch("https://backend.headpat.de/api/badges", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        body: formData,
-      });
+      console.log("response:", response);
 
       const responseData = await response.json();
       if (response.ok) {
-        //console.log("File uploaded successfully");
-        setIsUploading(false); // Set isUploading to false after the API call is complete
-        // Add the "Saved!" text to the form
-        alert("Saved!");
-        window.location.reload();
+        formData.append(
+          "data",
+          JSON.stringify({
+            users_permissions_user: userId,
+            displayname: displaynameRef.current?.value,
+            pronouns: pronounsRef.current?.value,
+            languages: selectedLanguages,
+            address: addressRef.current?.value,
+            city: cityRef.current?.value,
+            country: countryRef.current?.value,
+            state: stateRef.current?.value,
+            postalcode: postalcodeRef.current?.value,
+            deliver_at_eurofurence: deliver_at_eurofurence.checked,
+          })
+        );
+
+        setIsUploading(true); // Set isUploading to true before making the API call
+
+        const response = await fetch("https://backend.headpat.de/api/badges", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_DOMAIN_API_KEY}`,
+          },
+          body: formData,
+        });
+
+        console.log("response:", response);
+
+        const responseData = await response.json();
+        if (response.ok) {
+          console.log("File uploaded successfully");
+          setIsUploading(false); // Set isUploading to false after the API call is complete
+          // Add the "Saved!" text to the form
+          alert("Saved!");
+          window.location.reload();
+        } else {
+          console.error("Failed to upload file:", responseData);
+        }
       } else {
-        console.error("Failed to upload file:", responseData);
+        setInterval(() => {
+          setError("Du darfst dies nur einmal anfragen.");
+        }, 5000);
       }
     } catch (error) {
       console.error(error);
@@ -120,32 +182,107 @@ export default function BadgePageComponent() {
 
   return (
     <>
-      <main className="relative lg:min-h-full">
-        <div className="h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12 flex items-center">
-          <Image
-            src="/images/badgefront.webp"
-            alt="Badge Preview"
-            className="h-full w-full object-contain object-center max-h-[600px]"
-            width={600}
-            height={1000}
-          />
-        </div>
+      <form onSubmit={handleSubmit}>
+        <main className="relative lg:min-h-full">
+          <div className="h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12 flex items-center">
+            <div className="flex-none">
+              <img
+                src="/images/badgefront.webp"
+                alt="Badge Preview"
+                className="h-full w-full object-contain object-center max-h-[600px] rounded"
+              />
 
-        <div>
-          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
-            <div className="lg:col-start-2">
-              <h1 className="text-sm font-medium text-indigo-600">
-                Badge Request
-              </h1>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Danke für deine Überlegung!
-              </p>
-              <p className="mt-2 text-base text-gray-500">
-                Danke das du dich für einen Badge interessierst! Bitte fülle das
-                Formular aus und wir werden uns so schnell wie möglich bei dir
-                melden.
-              </p>
-              <form onSubmit={handleSubmit}>
+              <fieldset className="mt-4">
+                <legend className="text-xl font-semibold leading-6 text-white">
+                  Sprachen
+                </legend>
+                <div className="mt-4 divide-y divide-gray-200 border-b border-t border-gray-200">
+                  <div className="relative flex items-start py-4">
+                    <div className="min-w-0 flex-1 text-sm leading-6">
+                      <label
+                        htmlFor={`german`}
+                        className="select-none font-medium text-white"
+                      >
+                        German
+                      </label>
+                    </div>
+                    <div className="ml-3 flex h-6 items-center">
+                      <input
+                        id={`german`}
+                        name={`languages`}
+                        type="checkbox"
+                        value="German"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div className="relative flex items-start py-4">
+                    <div className="min-w-0 flex-1 text-sm leading-6">
+                      <label
+                        htmlFor={`english`}
+                        className="select-none font-medium text-white"
+                      >
+                        English
+                      </label>
+                    </div>
+                    <div className="ml-3 flex h-6 items-center">
+                      <input
+                        id={`english`}
+                        name={`languages`}
+                        type="checkbox"
+                        value="English"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <div className="relative flex items-start py-4">
+                    <div className="min-w-0 flex-1 text-sm leading-6">
+                      <label
+                        htmlFor={`dutch`}
+                        className="select-none font-medium text-white"
+                      >
+                        Dutch
+                      </label>
+                    </div>
+                    <div className="ml-3 flex h-6 items-center">
+                      <input
+                        id={`dutch`}
+                        name={`languages`}
+                        type="checkbox"
+                        value="Dutch"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                    </div>
+                  </div>
+                  <input
+                    type="hidden"
+                    id="selectedLanguages"
+                    name="selectedLanguages"
+                  />
+                </div>
+                {error && (
+                  <p className="mt-2 text-sm text-red-600" id="error">
+                    {error}
+                  </p>
+                )}
+              </fieldset>
+            </div>
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
+              <div className="lg:col-start-2">
+                <h1 className="text-sm font-medium text-indigo-600">
+                  Badge Request
+                </h1>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  Danke für deine Überlegung!
+                </p>
+                <p className="mt-2 text-base text-gray-500">
+                  Danke das du dich für einen Badge interessierst! Bitte fülle
+                  das Formular aus und wir werden uns so schnell wie möglich bei
+                  dir melden.
+                </p>
                 <ul
                   role="list"
                   className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-gray-500"
@@ -189,7 +326,7 @@ export default function BadgePageComponent() {
                     </div>
                     <div className="mt-1 align-middle text-center">
                       <p className="text-red-500">
-                        PNG, JPEG, GIF, SVG, TIFF, ICO, DVU
+                        PNG, JPEG, SVG, TIFF, ICO, DVU, WEBP
                       </p>
                       <p className="text-red-500 pt-2">Max. 512x512px, 1MB</p>
                     </div>
@@ -197,7 +334,7 @@ export default function BadgePageComponent() {
                 </ul>
 
                 <div className="sm:col-span-3 mb-4">
-                  <label className="block text-sm font-medium leading-6 text-white">
+                  <label className="block text-xl font-medium leading-6 text-white">
                     Bei Eurofurence abholen?
                   </label>
                   <div className="mt-2">
@@ -219,6 +356,19 @@ export default function BadgePageComponent() {
                       type="text"
                       name="displayname"
                       id="displayname"
+                      ref={displaynameRef}
+                      className="text-black rounded"
+                      required
+                    />
+                  </div>
+
+                  <div className="flex justify-between">
+                    <dt>Pronouns</dt>
+                    <input
+                      type="text"
+                      name="pronouns"
+                      id="pronouns"
+                      ref={pronounsRef}
                       className="text-black rounded"
                     />
                   </div>
@@ -231,6 +381,7 @@ export default function BadgePageComponent() {
                           type="text"
                           name="address"
                           id="address"
+                          ref={addressRef}
                           className="text-black rounded"
                         />
                       </div>
@@ -241,6 +392,7 @@ export default function BadgePageComponent() {
                           type="text"
                           name="city"
                           id="city"
+                          ref={cityRef}
                           className="text-black rounded"
                         />
                       </div>
@@ -253,8 +405,9 @@ export default function BadgePageComponent() {
                           Country
                         </label>
                         <select
-                          id="country"
                           name="country"
+                          id="country"
+                          ref={countryRef}
                           className="text-black appearance-none rounded"
                         >
                           <option
@@ -284,6 +437,7 @@ export default function BadgePageComponent() {
                           type="text"
                           name="state"
                           id="state"
+                          ref={stateRef}
                           className="text-black rounded"
                         />
                       </div>
@@ -294,6 +448,7 @@ export default function BadgePageComponent() {
                           type="text"
                           name="postalcode"
                           id="postalcode"
+                          ref={postalcodeRef}
                           className="text-black rounded"
                         />
                       </div>
@@ -318,11 +473,11 @@ export default function BadgePageComponent() {
                     {/* Show different text based on the upload state */}
                   </button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </form>
     </>
   );
 }
