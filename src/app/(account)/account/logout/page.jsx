154d24e24894bb3f39cc2e2c4ca30beef66a1058
Faye <dominic@stilma.net>
2024-03-02 00:18:38 +0100

    fix logout url

--- src/app/(account)/account/logout/page.jsx
+++ src/app/(account)/account/logout/page.jsx
@@ -1,28 +1,28 @@
 'use client'
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-export default function LogoutPage() {
-  const [error, setError] = useState(null);
+export default function LogoutPage () {
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/user/logoutUser`, {
-      method: "POST",
+    fetch(`/api/user/logoutUser`, {
+      method: 'POST',
     })
-    .then(response => {
-      if (!response.ok) { throw response; }
-      return response.json();  // we only get here if there is no error
-    })
-    .then(() => {
-      window.location.href = "/";
-    })
-    .catch(err => {
-      setError(err);
-    });
-  }, []);
+      .then(response => {
+        if (!response.ok) { throw response }
+        return response.json()  // we only get here if there is no error
+      })
+      .then(() => {
+        window.location.href = '/'
+      })
+      .catch(err => {
+        setError(err)
+      })
+  }, [])
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   }
 
-  return null;
+  return null
 }
