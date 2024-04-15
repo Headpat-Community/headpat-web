754fa1c9e69510ed2b0f353de1439070191d4fe1
Faye <dominic@stilma.net>
2024-04-15 20:14:26 +0200

    fix cookie

--- src/app/api/user/signin/route.js
+++ src/app/api/user/signin/route.js
@@ -1,50 +1,37 @@
 import { createAdminClient } from '../../../appwrite-session'
-import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export const runtime = 'edge'
 
 export async function POST(request) {
   const { account } = await createAdminClient()
+
   // if POST is not json, return 400
   if (request.headers.get('content-type') !== 'application/json') {
     return NextResponse.json({ error: 'Invalid content type' }, { status: 400 })
   }
+
   if (!request.body) {
     return NextResponse.json({ error: 'No body provided' }, { status: 400 })
   }
+
   const { email, password } = await request.json()
 
   try {
     const session = await account.createEmailPasswordSession(email, password)
 
-    cookies().set(
-      `session_${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`,
-      session.secret,
+    // Set cookie for headpat.de
+    const headpatCookie = `a_session_${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}=${session.secret}; Path=/; Domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN}; Max-Age=${session.expire}; Secure; HttpOnly; SameSite=Strict`
+
+    return NextResponse.json(
+      {},
       {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        maxAge: new Date(session.expire),
-        path: '/',
-        domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+        status: 200,
+        headers: {
+          'Set-Cookie': [headpatCookie],
+        },
       }
     )
-
-    cookies().set(
-      `a_session_${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`,
-      session.secret,
-      {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none',
-        maxAge: new Date(session.expire),
-        path: '/',
-        domain: 'api.headpat.de',
-      }
-    )
-
-    return NextResponse.json({}, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error: error }, { status: error.code })
   }
