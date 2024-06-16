9f3274485a67758380fb68446f55fc89cd5e9759
Faye <dominic@stilma.net>
2024-06-16 22:55:13 +0200

    fix error, fix runtime

--- src/app/[lang]/(user)/profile/page.tsx
+++ src/app/[lang]/(user)/profile/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from 'next/navigation'
 import { getUserData } from '@/utils/server-api/user/getUserData'
 
+export const runtime = 'edge'
+
 export default async function Profile({ params: { lang } }) {
   const user = await getUserData()
   return redirect(`/${lang}/user/${user.profileUrl}`)
