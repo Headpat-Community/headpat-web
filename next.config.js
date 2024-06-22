516e1e19f133ab6ea45fa37e3adfa65c5de38885
Faye <dominic@stilma.net>
2024-06-22 22:33:22 +0200

    test sentry size

--- next.config.js
+++ next.config.js
@@ -70,7 +70,7 @@ module.exports = withSentryConfig(module.exports, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+  widenClientFileUpload: false,
 
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
