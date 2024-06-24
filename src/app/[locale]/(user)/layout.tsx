c492afb7e2067ad8637da1d84eb1c64aff1897df
Faye <dominic@stilma.net>
2024-06-25 00:52:33 +0200

    fix translations

--- src/app/[locale]/(user)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/header/header-server'
-
-export default function LocaleLayout({ children, params: { locale } }) {
-  return (
-    <div>
-      <Header locale={locale}>{children}</Header>
-    </div>
-  )
-}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,27 @@
-export default async function LocaleLayout({ children }) {
-  return children
+import Header from '@/components/header/header-server'
+import { getTranslations } from 'next-intl/server'
+
+export default async function LocaleLayout({ children, params: { locale } }) {
+  const pageNames = await getTranslations({ locale, namespace: 'PageNames' })
+
+  return (
+    <div>
+      <Header
+        locale={locale}
+        home={pageNames('home')}
+        gallery={pageNames('gallery')}
+        events={pageNames('events')}
+        announcements={pageNames('announcements')}
+        users={pageNames('users')}
+        myprofile={pageNames('myprofile')}
+        legal={pageNames('legal')}
+        communities={pageNames('communities')}
+        account={pageNames('account')}
+        login={pageNames('login')}
+        logout={pageNames('logout')}
+      >
+        {children}
+      </Header>
+    </div>
+  )
 }
