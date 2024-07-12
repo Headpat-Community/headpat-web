ceab581ada4683be0aa3aed4d861dea230c9fcd6
Faye <faye@fayevr.dev>
2024-07-12 21:49:18 +0200

    add sorting

--- src/app/[locale]/(user)/notifications/page.tsx
+++ src/app/[locale]/(user)/notifications/page.tsx
@@ -34,63 +34,69 @@ export default async function Page() {
         className="mx-8 lg:mx-auto mb-4 mt-8 max-w-4xl divide-y divide-gray-100 shadow-sm ring-1 ring-black/95 dark:ring-white/95 sm:rounded-xl"
       >
         {notifications &&
-          notifications.documents.map(async (notification) => {
-            if (notification.type === 'newFollower') {
-              const user = await getUserDataSingle(notification.message)
+          notifications.documents
+            .sort(
+              (a, b) =>
+                new Date(b.$createdAt).getTime() -
+                new Date(a.$createdAt).getTime()
+            )
+            .map(async (notification) => {
+              if (notification.type === 'newFollower') {
+                const user = await getUserDataSingle(notification.message)
 
-              return (
-                <li
-                  key={notification.$id}
-                  className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50/90 dark:hover:bg-gray-50/10 sm:px-6"
-                >
-                  <div className="flex min-w-0 gap-x-4 items-center">
-                    <UserCard user={user} isChild={false}>
-                      <Link
-                        href={{
-                          pathname: '/user/[profileUrl]',
-                          params: { profileUrl: user.profileUrl },
-                        }}
-                      >
-                        <Avatar>
-                          <AvatarImage
-                            src={
-                              getAvatarImageUrlPreview(
-                                user.avatarId,
-                                'width=250&height=250'
-                              ) || null
-                            }
-                          />
-                          <AvatarFallback>
-                            {user.displayName.charAt(0).toUpperCase()}
-                          </AvatarFallback>
-                        </Avatar>
-                      </Link>
-                    </UserCard>
-                    <div className="min-w-0 flex-auto">
-                      <p className="mt-1 flex leading-5 text-gray-400 dark:text-gray-300">
-                        <UserCard user={user} isChild={false}>
-                          <Link
-                            href={{
-                              pathname: '/user/[profileUrl]',
-                              params: { profileUrl: user.profileUrl },
-                            }}
-                            className={'font-bold'}
-                          >
-                            {user.displayName}
-                          </Link>
-                          &nbsp;
-                        </UserCard>{' '}
-                        followed you! 🎉
-                      </p>
+                return (
+                  <li
+                    key={notification.$id}
+                    className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50/90 dark:hover:bg-gray-50/10 sm:px-6"
+                  >
+                    <div className="flex min-w-0 gap-x-4 items-center">
+                      <UserCard user={user} isChild={false}>
+                        <Link
+                          href={{
+                            pathname: '/user/[profileUrl]',
+                            params: { profileUrl: user.profileUrl },
+                          }}
+                        >
+                          <Avatar>
+                            <AvatarImage
+                              src={
+                                getAvatarImageUrlPreview(
+                                  user.avatarId,
+                                  'width=250&height=250'
+                                ) || null
+                              }
+                            />
+                            <AvatarFallback>
+                              {user.displayName.charAt(0).toUpperCase()}
+                            </AvatarFallback>
+                          </Avatar>
+                        </Link>
+                      </UserCard>
+                      <div className="min-w-0 flex-auto">
+                        <p className="mt-1 flex leading-5 text-gray-400 dark:text-gray-300">
+                          <UserCard user={user} isChild={false}>
+                            <Link
+                              href={{
+                                pathname: '/user/[profileUrl]',
+                                params: { profileUrl: user.profileUrl },
+                              }}
+                              className={'font-bold'}
+                            >
+                              {user.displayName}
+                            </Link>
+                            &nbsp;
+                          </UserCard>{' '}
+                          followed you! 🎉
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex shrink-0 items-center gap-x-4">
-                    {formatDate(new Date(notification.$createdAt))}
-                  </div>
-                </li>
-              )
-            }
-          })}
+                    <div className="flex shrink-0 items-center gap-x-4">
+                      {formatDate(new Date(notification.$createdAt))}
+                    </div>
+                  </li>
+                )
+              }
+            })}
       </ul>
     </PageLayout>
   )
