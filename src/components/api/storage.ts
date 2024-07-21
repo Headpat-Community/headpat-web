68d7f4aa6f670d43b2a59960c5f9ff799a297fc1
Faye <faye@fayevr.dev>
2024-07-21 17:41:45 +0200

    fix import

--- src/components/api/storage.ts
+++ src/components/api/storage.ts
@@ -1,9 +1,6 @@
 'use client'
-import { getCookie } from '@/utils/common'
 import { storage } from '@/app/appwrite-client'
 
 export function getFile(bucketId: string, fileId: string) {
-  const orgId = getCookie('orgId')
-  const bucketName = orgId + '-' + bucketId
-  return storage.getFile(`${bucketName}`, `${fileId}`)
+  return storage.getFile(`${bucketId}`, `${fileId}`)
 }
