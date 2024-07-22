fd01869a8d5e649fdbf30c869daeabf78438327f
Faye <faye@fayevr.dev>
2024-07-22 22:43:39 +0200

    add the "map"

--- src/app/[locale]/(main)/map/page.tsx
+++ src/app/[locale]/(main)/map/page.tsx
@@ -1,6 +1,8 @@
 import PageLayout from '@/components/pageLayout'
 import PageClient from './page.client'
 
+export const runtime = 'edge'
+
 export default async function Page() {
   return (
     <PageLayout title={'Map'}>
