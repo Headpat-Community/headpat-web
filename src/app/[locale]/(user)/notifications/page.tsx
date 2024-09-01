32187c84bbfd6caf7d8fbc505b7c1cd535aaa6dc
Faye <faye@fayevr.dev>
2024-09-01 13:22:37 +0200

    change notifications to functions

--- src/app/[locale]/(user)/notifications/page.tsx
+++ src/app/[locale]/(user)/notifications/page.tsx
@@ -12,7 +12,7 @@ export const runtime = 'edge'
 export default async function Page() {
   const notifications = await getUserNotifications()
 
-  if (notifications.documents.length === 0) {
+  if (notifications.length === 0) {
     return (
       <PageLayout title={'Notifications'}>
         <div className="mx-auto mb-4 mt-8 max-w-7xl">
@@ -33,70 +33,73 @@ export default async function Page() {
         role="list"
         className="mx-8 lg:mx-auto mb-4 mt-8 max-w-4xl divide-y divide-gray-100 shadow-sm ring-1 ring-black/95 dark:ring-white/95 sm:rounded-xl"
       >
-        {notifications &&
-          notifications.documents
-            .sort(
-              (a, b) =>
-                new Date(b.$createdAt).getTime() -
-                new Date(a.$createdAt).getTime()
-            )
-            .map(async (notification) => {
-              if (notification.type === 'newFollower') {
-                const user = await getUserDataSingle(notification.message)
-
-                return (
-                  <li
-                    key={notification.$id}
-                    className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50/90 dark:hover:bg-gray-50/10 sm:px-6"
-                  >
-                    <div className="flex min-w-0 gap-x-4 items-center">
-                      <UserCard user={user} isChild={false}>
-                        <Link
-                          href={{
-                            pathname: '/user/[profileUrl]',
-                            params: { profileUrl: user.profileUrl },
-                          }}
-                        >
-                          <Avatar>
-                            <AvatarImage
-                              src={
-                                getAvatarImageUrlPreview(
-                                  user.avatarId,
-                                  'width=250&height=250'
-                                ) || null
-                              }
-                            />
-                            <AvatarFallback>
-                              {user.displayName.charAt(0).toUpperCase()}
-                            </AvatarFallback>
-                          </Avatar>
-                        </Link>
-                      </UserCard>
-                      <div className="min-w-0 flex-auto">
-                        <p className="mt-1 flex leading-5 text-gray-400 dark:text-gray-300">
-                          <UserCard user={user} isChild={false}>
-                            <Link
-                              href={{
-                                pathname: '/user/[profileUrl]',
-                                params: { profileUrl: user.profileUrl },
-                              }}
-                              className={'font-bold'}
-                            >
-                              {user.displayName}
-                            </Link>
-                            &nbsp;
-                          </UserCard>{' '}
-                          followed you! 🎉
-                        </p>
-                      </div>
+        {notifications
+          .sort(
+            (a, b) =>
+              new Date(b.$createdAt).getTime() -
+              new Date(a.$createdAt).getTime()
+          )
+          .map(async (notification) => {
+            if (notification.type === 'newFollower') {
+              return (
+                <li
+                  key={notification.$id}
+                  className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50/90 dark:hover:bg-gray-50/10 sm:px-6"
+                >
+                  <div className="flex min-w-0 gap-x-4 items-center">
+                    <UserCard user={notification.userData} isChild={false}>
+                      <Link
+                        href={{
+                          pathname: '/user/[profileUrl]',
+                          params: {
+                            profileUrl: notification.userData.profileUrl,
+                          },
+                        }}
+                      >
+                        <Avatar>
+                          <AvatarImage
+                            src={
+                              getAvatarImageUrlPreview(
+                                notification.userData.avatarId,
+                                'width=250&height=250'
+                              ) || null
+                            }
+                          />
+                          <AvatarFallback>
+                            {notification.userData.displayName
+                              .charAt(0)
+                              .toUpperCase()}
+                          </AvatarFallback>
+                        </Avatar>
+                      </Link>
+                    </UserCard>
+                    <div className="min-w-0 flex-auto">
+                      <p className="mt-1 flex leading-5 text-gray-400 dark:text-gray-300">
+                        <UserCard user={notification.userData} isChild={false}>
+                          <Link
+                            href={{
+                              pathname: '/user/[profileUrl]',
+                              params: {
+                                profileUrl: notification.userData.profileUrl,
+                              },
+                            }}
+                            className={'font-bold'}
+                          >
+                            {notification.userData.displayName}
+                          </Link>
+                          &nbsp;
+                        </UserCard>{' '}
+                        followed you! 🎉
+                      </p>
                     </div>
-                    <div className="flex shrink-0 items-center gap-x-4">
-                      {formatDate(new Date(notification.$createdAt))}
-                    </div>
-                  </li>
-                )
-              }
-            })}
+                  </div>
+                  <div className="flex shrink-0 items-center gap-x-4">
+                    {formatDate(new Date(notification.$createdAt))}
+                  </div>
+                </li>
+              )
+            }
+          })}
       </ul>
     </PageLayout>
   )
--- src/utils/server-api/notifications/getUserNotifications.ts
+++ src/utils/server-api/notifications/getUserNotifications.ts
@@ -1,9 +1,23 @@
 import { createSessionServerClient } from '@/app/appwrite-session'
 import { Notifications } from '@/utils/types/models'
 import { unstable_noStore } from 'next/cache'
+import { ExecutionMethod } from 'node-appwrite'
 
-export async function getUserNotifications(): Promise<Notifications.NotificationsType> {
+export async function getUserNotifications(): Promise<
+  Notifications.NotificationsDocumentsType[]
+> {
   unstable_noStore()
-  const { databases } = await createSessionServerClient()
-  return await databases.listDocuments('hp_db', 'user-notifications')
+  const { functions } = await createSessionServerClient()
+  const data = await functions.createExecution(
+    'user-endpoints',
+    '',
+    false,
+    `/user/notifications`,
+    ExecutionMethod.GET
+  )
+  const response: Notifications.NotificationsDocumentsType[] = JSON.parse(
+    data.responseBody
+  )
+  console.log(response)
+  return response
 }
