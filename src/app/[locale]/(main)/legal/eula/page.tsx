faf38aef472e01837c136890cca746784e756884
Faye <faye@fayevr.dev>
2024-09-04 00:41:16 +0200

    fix edge

--- src/app/[locale]/(main)/legal/eula/page.tsx
+++ src/app/[locale]/(main)/legal/eula/page.tsx
@@ -1,6 +1,12 @@
 import Eula from './eula.mdx'
 import PageLayout from '@/components/pageLayout'
 
+export const metadata = {
+  title: 'EULA',
+}
+
+export const runtime = 'edge'
+
 export default function Page() {
   return (
     <PageLayout title={'EULA'}>
