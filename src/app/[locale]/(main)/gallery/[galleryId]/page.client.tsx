bfcced9ca1ec764da3728afcfc9662083751e4c6
Faye <faye@fayevr.dev>
2024-09-06 15:38:57 +0200

    fix nsfw image

--- src/app/[locale]/(main)/gallery/[galleryId]/page.client.tsx
+++ src/app/[locale]/(main)/gallery/[galleryId]/page.client.tsx
@@ -69,7 +69,7 @@ export default function PageClient({ galleryId }: { galleryId: string }) {
         <div className="flex flex-wrap items-center justify-center gap-4 p-8">
           <div>
             <div className="flex flex-wrap items-start">
-              {!image.nsfw && (
+              {!image.nsfw && !current.prefs.nsfw && (
                 <div className="mb-4 mr-4 flex sm:mt-4 md:mb-0">
                   <Link
                     href={'/gallery'}
@@ -79,7 +79,7 @@ export default function PageClient({ galleryId }: { galleryId: string }) {
                   </Link>
                 </div>
               )}
-              {image.nsfw ? (
+              {image.nsfw && !current.prefs.nsfw ? (
                 <div className="fixed inset-0 flex items-center justify-center">
                   {/* Semi-transparent overlay */}
                   <div
