35eb0e51fa1f4e769b2a18758cefe72d77f30731
Faye <faye@fayevr.dev>
2024-09-27 01:50:47 +0200

    add runtime

--- src/app/api/device/route.ts
+++ src/app/api/device/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function GET(request: NextRequest) {
   const { device } = userAgent(request)
   return NextResponse.json(device)
