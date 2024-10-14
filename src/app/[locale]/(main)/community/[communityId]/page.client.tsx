6bcff7e851c678703a5471762bbbe5029c91576a
Faye <faye@fayevr.dev>
2024-10-15 01:39:26 +0200

    fix community page

--- src/app/[locale]/(main)/community/[communityId]/page.client.tsx
+++ src/app/[locale]/(main)/community/[communityId]/page.client.tsx
@@ -5,7 +5,6 @@ import { functions } from '@/app/appwrite-client'
 import { ExecutionMethod } from 'node-appwrite'
 import { toast } from 'sonner'
 import { Skeleton } from '@/components/ui/skeleton'
-import ContextMenuProfile from '@/components/user/contextMenuProfile'
 import Image from 'next/image'
 import {
   getCommunityAvatarUrlView,
@@ -63,128 +62,120 @@ export default function PageClient({
   } else if (!community.communitySettings.hasPublicPage) {
     return notFound()
   } else if (!current?.prefs?.nsfw && community.communitySettings.nsfw) {
-    if (current)
-      return (
-        <PageLayout title={community?.name || 'Community'}>
-          <NoAccessNsfw />
-        </PageLayout>
-      )
-  }
-
-  if (current)
     return (
       <PageLayout title={community?.name || 'Community'}>
-        <ContextMenuProfile>
-          <main className={'max-w-7xl mx-auto'}>
-            <>
-              {/* Header */}
-              {community.bannerId && (
-                <header className={'p-0 lg:p-8'}>
-                  <div className={''}>
-                    <Image
-                      src={getCommunityBannerUrlPreview(
-                        community.bannerId,
-                        'width=1200&height=250&output=webp'
-                      )}
-                      alt={'User Banner'}
-                      className={
-                        'rounded-md object-cover max-w-[1200px] max-h-[250px] px-8 lg:px-0 mt-8 lg:mt-0 w-full h-auto'
-                      }
-                      width={1200}
-                      height={250}
-                      priority={true}
-                    />
-                  </div>
-                </header>
-              )}
-
-              {/* Grid */}
-              <div
-                className={cn(
-                  'grid grid-cols-1 gap-x-2 md:gap-x-4 lg:gap-x-8 gap-y-8 lg:grid-cols-3 xl:gap-x-10 pr-8 pl-8 md:grid-cols-2',
-                  community.bannerId ? 'mt-0' : 'mt-8'
-                )}
-              >
-                {/* Left */}
-                <div
-                  className={
-                    'col-span-3 lg:col-span-1 lg:mt-0 mt-4 rounded-t-xl rounded-b-xl md:col-span-1'
-                  }
-                >
-                  <AspectRatio ratio={2 / 2}>
-                    <Image
-                      src={getCommunityAvatarUrlView(community.avatarId)}
-                      alt={'User Avatar'}
-                      className={'object-contain rounded-xl'}
-                      fill={true}
-                      priority={true}
-                      unoptimized
-                    />
-                  </AspectRatio>
-                </div>
-                {/* Center */}
-                <Card
-                  className={
-                    'col-span-3 border-none md:col-span-1 lg:col-span-2'
-                  }
-                >
-                  <CardHeader>
-                    <div
-                      className={
-                        'grid grid-cols-1 xl:grid-cols-2 gap-4 xl:gap-0'
-                      }
-                    >
-                      <CardTitle className={'col-span-1'}>
-                        {community.name}
-                      </CardTitle>
-                      <FollowerButton
-                        displayName={community.name}
-                        communityId={communityId}
-                      />
-                    </div>
-                    <div className={'grid grid-cols-2'}>
-                      <CardDescription>{community.status}</CardDescription>
-                    </div>
-                    <CardDescription className={'flex pt-4 gap-4'}>
-                      <Link
-                        href={{
-                          pathname: '/community/[communityId]/followers',
-                          params: { communityId },
-                        }}
-                      >
-                        <Button variant={'link'} className={'p-0'}>
-                          {isLoading ? (
-                            <Skeleton className={'w-full h-10'} />
-                          ) : (
-                            <p>
-                              <span className={'font-bold text-foreground'}>
-                                {community.followersCount}
-                              </span>{' '}
-                              Follower
-                              {community.followersCount !== 1 ? 's' : ''}
-                            </p>
-                          )}
-                        </Button>
-                      </Link>
-                    </CardDescription>
-                  </CardHeader>
-                  <Separator className={'mb-6'} />
-                  <CardContent>
-                    <div className={'border border-ring p-8 rounded-xl mt-8'}>
-                      <div className={'flex flex-wrap items-center'}>
-                        <p>{community.description || 'Nothing here yet!'}</p>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-                {/* Right */}
-                {/* Posts here */}
-              </div>
-            </>
-          </main>
-        </ContextMenuProfile>
+        <NoAccessNsfw />
       </PageLayout>
     )
+  }
+
+  return (
+    <PageLayout title={community?.name || 'Community'}>
+      <main className={'max-w-7xl mx-auto'}>
+        <>
+          {/* Header */}
+          {community.bannerId && (
+            <header className={'p-0 lg:p-8'}>
+              <div className={''}>
+                <Image
+                  src={getCommunityBannerUrlPreview(
+                    community.bannerId,
+                    'width=1200&height=250&output=webp'
+                  )}
+                  alt={'User Banner'}
+                  className={
+                    'rounded-md object-cover max-w-[1200px] max-h-[250px] px-8 lg:px-0 mt-8 lg:mt-0 w-full h-auto'
+                  }
+                  width={1200}
+                  height={250}
+                  priority={true}
+                />
+              </div>
+            </header>
+          )}
+
+          {/* Grid */}
+          <div
+            className={cn(
+              'grid grid-cols-1 gap-x-2 md:gap-x-4 lg:gap-x-8 gap-y-8 lg:grid-cols-3 xl:gap-x-10 pr-8 pl-8 md:grid-cols-2',
+              community.bannerId ? 'mt-0' : 'mt-8'
+            )}
+          >
+            {/* Left */}
+            <div
+              className={
+                'col-span-3 lg:col-span-1 lg:mt-0 mt-4 rounded-t-xl rounded-b-xl md:col-span-1'
+              }
+            >
+              <AspectRatio ratio={2 / 2}>
+                <Image
+                  src={getCommunityAvatarUrlView(community.avatarId)}
+                  alt={'User Avatar'}
+                  className={'object-contain rounded-xl'}
+                  fill={true}
+                  priority={true}
+                  unoptimized
+                />
+              </AspectRatio>
+            </div>
+            {/* Center */}
+            <Card
+              className={'col-span-3 border-none md:col-span-1 lg:col-span-2'}
+            >
+              <CardHeader>
+                <div
+                  className={'grid grid-cols-1 xl:grid-cols-2 gap-4 xl:gap-0'}
+                >
+                  <CardTitle className={'col-span-1'}>
+                    {community.name}
+                  </CardTitle>
+                  <FollowerButton
+                    displayName={community.name}
+                    communityId={communityId}
+                  />
+                </div>
+                <div className={'grid grid-cols-2'}>
+                  <CardDescription>{community.status}</CardDescription>
+                </div>
+                <CardDescription className={'flex pt-4 gap-4'}>
+                  <Link
+                    href={{
+                      pathname: '/community/[communityId]/followers',
+                      params: { communityId },
+                    }}
+                  >
+                    <Button variant={'link'} className={'p-0'}>
+                      {isLoading ? (
+                        <Skeleton className={'w-full h-10'} />
+                      ) : (
+                        <p>
+                          <span className={'font-bold text-foreground'}>
+                            {community.followersCount}
+                          </span>{' '}
+                          Follower
+                          {community.followersCount !== 1 ? 's' : ''}
+                        </p>
+                      )}
+                    </Button>
+                  </Link>
+                </CardDescription>
+              </CardHeader>
+              <Separator className={'mb-6'} />
+              <CardContent>
+                <div className={'border border-ring p-8 rounded-xl mt-8'}>
+                  <div className={'flex flex-wrap items-center'}>
+                    <p>{community.description || 'Nothing here yet!'}</p>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            {/* Right */}
+            {/* Posts here */}
+          </div>
+        </>
+      </main>
+    </PageLayout>
+  )
 }
 
 export function FollowerButton({ displayName, communityId }) {
