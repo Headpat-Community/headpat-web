cb5bf5a40618cf62ff1e0d0b8f664a6e616a98c9
Faye <faye@fayevr.dev>
2024-12-07 21:30:24 +0100

    sexy button

---
+++ src/components/ui/custom/highlight.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+export const Highlight = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <span
+      className={cn(
+        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+        className
+      )}
+    >
+      {children}
+    </span>
+  );
+};
