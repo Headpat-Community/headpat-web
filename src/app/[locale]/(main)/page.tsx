4b6a38714ff7e70f5f98d7c387c423663e1c0987
Faye <faye@fayevr.dev>
2024-12-07 21:50:27 +0100

    colors

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -105,11 +105,11 @@ export default async function Home({ params }) {
               <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
                 <div className="mt-24 sm:mt-32 lg:mt-16 flex space-x-6 items-center">
                   <Link href="/app">
-                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl dark:shadow-zinc-900 shadow-zinc-100 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
                       <span className="absolute inset-0 overflow-hidden rounded-full">
                         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                       </span>
-                      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
                         <span>{main('checkapp')}</span>
                         <svg
                           fill="none"
