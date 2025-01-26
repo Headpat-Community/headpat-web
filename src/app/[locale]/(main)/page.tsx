2de689093f68350ef24bf006318036a3c40656f9
Faye <faye@fayevr.dev>
2025-01-26 18:00:58 +0100

    update packages

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -135,13 +135,11 @@ export default async function Home({ params }) {
               </div>
               <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                 <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-lg">
-                  <Image
+                  <img
                     alt="App screenshot"
                     src="/images/headpat_frontpage.webp"
                     draggable={false}
-                    priority
-                    width={1580}
-                    height={1848}
+                    fetchPriority={'high'}
                     className="w-[30rem] md:w-[50rem] lg:w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
                   />
                 </div>
