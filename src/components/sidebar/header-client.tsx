2a6e5da1da79885a6364dd833c16b9590544be3c
Faye <faye@fayevr.dev>
2025-06-14 02:34:19 +0200

    fix: padding

--- src/components/sidebar/header-client.tsx
+++ src/components/sidebar/header-client.tsx
@@ -42,7 +42,7 @@ function HeaderProvider({ children }: { children: React.ReactNode }) {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 -mr-4" />
+            <SidebarTrigger className="-ml-1 -mr-4 p-4" />
             <Separator orientation="vertical" />
             <ChangeLanguage />
             <ThemeToggle />
